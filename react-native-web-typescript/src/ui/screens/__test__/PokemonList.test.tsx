34b0650d71742c4c1ee83ce665fa6b86fc6eb721
iraticasi <irati.casi@540deg.com>
2021-07-22 09:20:37 +0200

    first test

---
+++ react-native-web-typescript/src/ui/screens/__test__/PokemonList.test.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {PokemonList} from '../PokemonList';
+
+it('should render correctly', () => {
+    renderer.create(
+        <PokemonList/>
+    );
+});
