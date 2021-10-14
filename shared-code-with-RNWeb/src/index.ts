fcd2dff2dbb2e0ba66a736580813caa36db26bf7
iraticasi <irati.casi@540deg.com>
2021-10-14 09:43:28 +0200

    change appnames and icon

--- shared-code-with-RNWeb/src/index.ts
+++ shared-code-with-RNWeb/src/index.ts
@@ -1,9 +1,9 @@
 import { AppRegistry, Platform } from 'react-native'
 import { App } from './App'
 
-AppRegistry.registerComponent('Pokedex', () => App)
+AppRegistry.registerComponent('RNWeb', () => App)
 if (Platform.OS === 'web') {
-  AppRegistry.runApplication('Pokedex', {
+  AppRegistry.runApplication('RNWeb', {
     rootTag: document.getElementById('root')
   })
 }
--- shared-code-with-uikit-bridge/src/index.native.tsx
+++ shared-code-with-uikit-bridge/src/index.native.tsx
@@ -1,4 +1,4 @@
 import {AppRegistry} from 'react-native';
 import {App} from './App';
 
-AppRegistry.registerComponent('Pokedex', () => App);
+AppRegistry.registerComponent('UIKit', () => App);
